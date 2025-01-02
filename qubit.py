da091cfea0bbff78f72ddded15ac9bc87a41c03d
FallAngel <fallangel@protonmail.com>
2025-01-02 09:29:16 -0300

    Add comparison methods and pretty print methods

--- qubit.py
+++ qubit.py
@@ -34,3 +34,22 @@ class Qubit:
             self.amps = rng.choice([zero.amps, one.amps], p=probs)
         self.is_collapsed = True
         return self
+
+    def is_zero(self) -> bool:
+        return self == Qubit.zero()
+    
+    def is_one(self) -> bool:
+        return self == Qubit.one()
+
+    def __eq__(self, other) -> bool:
+        return self.amps[0] == other.amps[0] and self.amps[1] == other.amps[1]
+    
+    def __str__(self):
+        if self.is_zero():
+            return '|0>'
+        if self.is_one():
+            return '|1>'
+        return f'Qubit({self.amps})'
+    
+    def __repr__(self):
+        return f'Qubit(is_collapsed={self.is_collapsed}, amps={self.amps})'
